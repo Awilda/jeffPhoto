f228eef92f9cf2da982f3ddcaa3a3f1f6de2b380
Awilda <awildacantres@gmail.com>
2017-07-18 23:23:04 -0400

    Fix fotorama load

--- js/app.js
+++ js/app.js
@@ -1,36 +1,42 @@
-$(document).ready(function() {
-	var $gallery = $("#gallery").fotorama(),
-		fotorama = $gallery.data('fotorama');
-		fotorama.load ([
-			{img: 'images/jj3.jpg'},
-			{img: 'images/jj4.jpg'},
-			{img: 'images/jj5.jpg'},
-			{img: 'images/jj7.jpg'},
-			{img: 'images/jj8.jpg'},
-			{img: 'images/jj9.jpg'},
-			{img: 'images/jj10.jpg'},
-			{img: 'images/jj11.jpg'},
-			{img: 'images/jj12.jpg'},
-			{img: 'images/jj13.jpg'},
-			{img: 'images/jj14.jpg'},
-			{img: 'images/jj15.jpg'},
-			{img: 'images/jj16.jpg'},
-			{img: 'images/jj17.jpg'},
-			{img: 'images/jj18.jpg'},
-			{img: 'images/jj19.jpg'},
-			{img: 'images/jj20.jpg'},
-			{img: 'images/jj21.jpg'},
-			{img: 'images/jj22.jpg'},
-			{img: 'images/jj23.jpg'},
-			{img: 'images/jj24.jpg'},
-			{img: 'images/jj25.jpg'},
-			{img: 'images/jj26.jpg'},
-			{img: 'images/jj27.jpg'},
-			{img: 'images/jj28.jpg'},
-			{img: 'images/jj29.jpg'},
-			{img: 'images/jj30.jpg'},
-			{img: 'images/jj31.jpg'},
-			{img: 'images/jj32.jpg'}
-		]);
+// $(document).ready(function() {
+// 	var $gallery = $("#gallery").fotorama(),
+// 		fotorama = $gallery.data('fotorama');
+// 		fotorama.load ([
 
-});
+$(document).ready(function() {
+ 	var $gallery = 
+ 	$("#gallery").fotorama({
+ 		data: [
+ 			{img: 'images/jj3.jpg'},
+ 			{img: 'images/jj4.jpg'},
+ 			{img: 'images/jj5.jpg'},
+ 			{img: 'images/jj7.jpg'},
+ 			{img: 'images/jj8.jpg'},
+ 			{img: 'images/jj9.jpg'},
+  		{img: 'images/jj10.jpg'},
+ 			{img: 'images/jj11.jpg'},
+ 			{img: 'images/jj12.jpg'},
+ 			{img: 'images/jj13.jpg'},
+ 			{img: 'images/jj14.jpg'},
+ 			{img: 'images/jj15.jpg'},
+ 			{img: 'images/jj16.jpg'},
+  		{img: 'images/jj17.jpg'},
+ 			{img: 'images/jj18.jpg'},
+ 			{img: 'images/jj19.jpg'},
+ 			{img: 'images/jj20.jpg'},
+ 			{img: 'images/jj21.jpg'},
+ 			{img: 'images/jj22.jpg'},
+ 			{img: 'images/jj23.jpg'},
+ 			{img: 'images/jj24.jpg'},
+ 			{img: 'images/jj25.jpg'},
+ 			{img: 'images/jj26.jpg'},
+ 			{img: 'images/jj27.jpg'},
+ 			{img: 'images/jj28.jpg'},
+ 			{img: 'images/jj29.jpg'},
+ 			{img: 'images/jj30.jpg'},
+ 			{img: 'images/jj31.jpg'},
+ 			{img: 'images/jj32.jpg'}
+ 		]
+ 	});
+ 
+ });
